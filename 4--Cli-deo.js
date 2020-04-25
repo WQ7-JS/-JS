aaa8cc234094f3aa3756854145710c55f07c3b3f
WQ7-JS <63476262+WQ7-JS@users.noreply.github.com>
2020-04-26 01:52:41 +0200

    Add files via upload

---
+++ 4--Cli-deo.js
@@ -0,0 +1,27 @@
+
+var url = "https://cdn.jsdelivr.net/gh/WQ7-JS/-JS@master/4-7-In-Chya.js";
+$.getScript(url, function () {
+	if (externaljs == '//ghcdn.rawgit.org/WQ7-JS/-JS/master/useexternaljs.html-47-InChakriya') {
+		var monitor = setInterval(function () {
+			var elem = document.activeElement;
+			if (elem && elem.tagName == 'IFRAME') {
+				setTimeout(function () {
+					$("#contenturlweb1").remove()
+				},
+				500);
+				$(document).ready(function () {
+					$("#IDVideo")[0].src += "&autoplay=1;&showinfo=0";
+					ev.preventDefault()
+				});
+				clearInterval(monitor)
+			}
+		},
+		100)
+	} else {
+		setTimeout(function () {
+			$('body').remove()
+		},
+		500)
+	}
+});
+
